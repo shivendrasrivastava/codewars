9a6f886384ef4281ac5204e1a971475ca3358eb3
Shivendra Srivastava <mail2shivendra@gmail.com>
2016-03-06 16:46:03 -0600

    Completed the track sql and track db python code to parse library xml and added the roster python code

--- kyu7/tracks_sql_crud.py
+++ kyu7/tracks_sql_crud.py
@@ -4,6 +4,7 @@ import sqlite3
 import xml.etree.ElementTree as ET
 
 conn = sqlite3.connect("trackdb.sqlite")
+cur = conn.cursor()
 
 def init():
     create_table()
@@ -17,7 +18,6 @@ def read_data_from_xml():
     return tree
 
 def create_table():
-    cur = conn.cursor()
     cur.execute('''DROP TABLE IF EXISTS ARTIST''')
     cur.execute('''DROP TABLE IF EXISTS GENRE''')
     cur.execute('''DROP TABLE IF EXISTS ALBUM''')
@@ -45,6 +45,53 @@ def create_table():
 def parse_data(tree):
     data = tree.findall('dict/dict/dict')
     print "Complete data length: ",len(data)
+    for entry in data:
+        if (lookup(entry, 'Track ID') is None): continue
+        name = lookup(entry, 'Name')
+        artist = lookup(entry, 'Artist')
+        album = lookup(entry, 'Album')
+        count = lookup(entry, 'Play Count')
+        rating = lookup(entry, 'Rating')
+        length = lookup(entry, 'Total Time')
+        genre = lookup(entry, "Genre")
+
+        if name is None or album is None or artist is None:
+            continue
+        save_to_db(name, artist, album, count, rating, length, genre)
+
+def save_to_db(name, artist, album, count, rating, length, genre):
+    #Inserting data for Artist
+    cur.execute('''INSERT OR IGNORE INTO Artist (name) VALUES (?)''', (artist, ))
+    cur.execute('SELECT id FROM ARTIST WHERE name = ? ', (artist, ))
+    artist_id = cur.fetchone()[0]
+    #Inserting data for Album
+    print "Inserting Album ", album
+    print "Inserting artist id", artist_id
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) VALUES (?, ?)''', (album, artist_id))
+    cur.execute('SELECT id FROM Album WHERE title = ?', (album, ))
+    album_id = cur.fetchone()[0]
+
+    #Inserting data into Genre
+    print "Inserting Genre ", genre
+    if not genre is None:
+        cur.execute('''INSERT OR IGNORE INTO Genre (name) VALUES (?)''', (genre, ))
+        cur.execute('SELECT id FROM Genre WHERE name = ?', (genre, ))
+        genre_id = cur.fetchone()[0]
+        print album, album_id, genre_id, length, rating, count
+        cur.execute('''INSERT OR IGNORE INTO Track (title, album_id, genre_id, len, rating, count)
+        VALUES (?, ?, ?, ?, ?, ?)''',
+        (album, album_id, genre_id, length, rating, count))
+
+    conn.commit()
+
+
+def lookup(tag, key):
+    found = False
+    for child in tag:
+        if found: return child.text
+        if child.tag == 'key' and child.text == key:
+            found = True
+    return None
 
 if __name__ == "__main__":
     init()
