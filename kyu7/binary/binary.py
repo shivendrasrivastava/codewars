1f78b1cc515affe4d2a38a65f93221cc94d8d056
Shivendra Srivastava <mail2shivendra@gmail.com>
2016-10-08 22:23:37 -0500

    Added question for binary problem hackerrank

--- kyu7/binary/binary.py
+++ kyu7/binary/binary.py
@@ -1,3 +1,29 @@
+'''
+Objective
+Today, we are working with binary numbers. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Given a base- integer, , convert it to binary (base-). Then find and print the base- integer denoting the maximum number of consecutive 's in 's binary representation.
+
+Input Format
+
+A single integer, .
+
+Constraints
+
+Output Format
+
+Print a single base- integer denoting the maximum number of consecutive 's in the binary representation of .
+
+Explanation
+
+Sample Case 1:
+The binary representation of 5 is 101 , so the maximum number of consecutive 1's is 1.
+
+Sample Case 2:
+The binary representation of 13 is 1101, so the maximum number of consecutive 1's is 2.
+'''
+
 __author__ = "Shiven"
 
 def base2(n):
