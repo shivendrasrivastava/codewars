c9c3396ff1a678337855315d8199f0f7f6d6ad3c
Shivendra Srivastava <mail2shivendra@gmail.com>
2016-10-13 21:08:24 -0500

    Added project euler problem 1 solution

--- projecteuler/multiples-unittest.py
+++ projecteuler/multiples-unittest.py
@@ -2,10 +2,19 @@ __author__ = "Shiven"
 
 import unittest
 from unittest.mock import patch
-from multiples import sum_multiples
+from multiples import sum_multiples, sum_multiples_fast
 from io import StringIO
+import cProfile
+import time
 
 class TestMultiples(unittest.TestCase):
+
+    def setUp(self):
+        self.startTime = time.time()
+
+    def tearDown(self):
+        t = time.time() - self.startTime
+        print("%s: %.3f" % (self.id(), t))
 
     def test_multiples_1(self):
         self.assertEqual(sum_multiples(10), 23)
@@ -13,5 +22,9 @@ class TestMultiples(unittest.TestCase):
     def test_multiples_2(self):
         self.assertEqual(sum_multiples(1000), 233168)
 
+    def test_multiple_3(self):
+        self.assertEqual(sum_multiples_fast(1000), 233168)
+
 if __name__ == "__main__":
-    unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestMultiples)
+    unittest.TextTestRunner(verbosity=0).run(suite)
--- projecteuler/multiples.py
+++ projecteuler/multiples.py
@@ -14,3 +14,9 @@ def sum_multiples(n):
             total += x
     print(total)
     return total
+
+def sum_multiples_fast(n):
+    return int(divisible(3, n-1) + divisible(5, n-1) - divisible(15, n-1))
+
+def divisible(n, p):
+    return n * (p/n) * ((p/n) + 1)/2
